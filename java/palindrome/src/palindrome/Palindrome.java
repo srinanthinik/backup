8ad631ff1add84b4bfc3b78e11bfa2885ace2c7d
srinanthinik <110449513+srinanthinik@users.noreply.github.com>
2022-08-17 16:27:00 +0530

    Add files via upload

--- java/palindrome/src/palindrome/Palindrome.java
+++ java/palindrome/src/palindrome/Palindrome.java
@@ -2,15 +2,7 @@ package palindrome;
 
 import java.util.Scanner;
 
-public class Palindrome {
-public static void main (String[]args)
-{
-	String original,reverse = "";
-	Scanner scan = new Scanner(System.in);
-	System.out.println("enter the string/number to check if it is palindrome or not:");
-	original = scan.nextLine();
-	int length = original.length();
-	for (int i = length-1; i>=0; i--) 
+cvb nvghmj
 		reverse = reverse+ original.charAt(i);
 	
 	if(original.equals(reverse)) 
